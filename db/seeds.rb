61b39ef77058e00e7429afc5b79e23908d8a98bd
senaygui <senaygeletaw@gmail.com>
2023-02-20 17:44:50 +0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@gmail.com', password: '1234567', password_confirmation: '1234567') if Rails.env.development?
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(first_name: "fenet",last_name: "fenet", email: "fenet@gmail.com", password: "password", password_confirmation: "password", role: "admin") if Rails.env.development?
+
+
+AdminUser.create!(email: "#{Rails.application.credentials.production[:admin_email]}", 
+									password: "#{Rails.application.credentials.production[:seed_password]}",password_confirmation: "#{Rails.application.credentials.production[:seed_password]}") if Rails.env.production?
